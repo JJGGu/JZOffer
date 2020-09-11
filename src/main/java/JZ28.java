84a1c12cca66b18f9dc435bcddb73e2e5bfcf22a
JJGGu <haoranxu1996@163.com>
2020-09-11 15:45:36 +0800

    JZ28

---
+++ src/main/java/JZ28.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @program: JZOffer
+ * @description: 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+ * @author: JJGGu
+ * @create: 2020-09-11 15:26
+ **/
+public class JZ28 {
+}
