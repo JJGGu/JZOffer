499fca44e09010992a1f45aa92fa6b55c35902e0
JJGGu <haoranxu1996@163.com>
2020-09-14 21:23:57 +0800

    JZ35

---
+++ src/main/java/JZ35.java
@@ -0,0 +1,15 @@
+/**
+ * @program: JZOffer
+ * @description: 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组,求出这个数组中的逆序对的总数P。并将P对1000000007取模的结果输出。 即输出P%1000000007
+ * @author: JJGGu
+ * @create: 2020-09-14 11:42
+ **/
+public class JZ35 {
+    public int InversePairs(int [] array) {
+
+        return 0;
+    }
+
+    // 归并排序参考 https://blog.csdn.net/k_koris/article/details/80508543
+    // 这个题暂时跳过
+}
