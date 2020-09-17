3a3a217157d3b99c284abcc1769b30ff66381cd4
JJGGu <haoranxu1996@163.com>
2020-09-17 10:22:49 +0800

    JZ58

--- src/main/java/JZ58.java
+++ src/main/java/JZ58.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @program: JZOffer
  * @description: 请实现一个函数，用来判断一棵二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
@@ -5,7 +8,32 @@
  * @create: 2020-09-16 22:08
  **/
 public class JZ58 {
-    public class TreeNode {
+    public static void main(String[] args) {
+        List<TreeNode> list = new ArrayList<>();
+        int [] x= {8, 6, 9,5, 7, 7, 5};
+        for (int i = 0; i < x.length; i++) {
+            list.add(new TreeNode(x[i]));
+        }
+
+        list.get(0).left = list.get(1);
+        list.get(0).right = list.get(2);
+        list.get(1).left = list.get(3);
+        list.get(1).right = list.get(4);
+        list.get(2).left = list.get(5);
+        list.get(2).right = list.get(6);
+        list.get(3).left = null;
+        list.get(3).right = null;
+        list.get(4).left = null;
+        list.get(4).right = null;
+        list.get(5).left = null;
+        list.get(5).right = null;
+        list.get(6).left = null;
+        list.get(6).right = null;
+
+        boolean symmetrical = isSymmetrical(list.get(0));
+        System.out.println(symmetrical);
+    }
+    public static class TreeNode {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
@@ -16,8 +44,21 @@ public class JZ58 {
         }
 
     }
-    boolean isSymmetrical(TreeNode pRoot)
-    {
-        
+    static boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+        return isSame(pRoot.left, pRoot.right);
     }
+    static boolean isSame(TreeNode r1, TreeNode r2) {
+        if (r1 == null) {
+            return r2 == null;
+        }
+        if (r2 == null) {
+            return false;
+        }
+        boolean a = r1.val == r2.val;
+        return a && isSame(r1.left, r2.right) && isSame(r1.right, r2.left);
+    }
+
 }
--- src/main/java/MyBinaryTree.java
+++ src/main/java/MyBinaryTree.java
@@ -5,7 +5,7 @@
  * @create: 2020-09-07 21:08
  **/
 public class MyBinaryTree {
-    private TreeNode root = new TreeNode();
+    public TreeNode root = new TreeNode();
     public static class TreeNode {
         int val;
         TreeNode left;
@@ -18,4 +18,41 @@ public class MyBinaryTree {
     }
 
 
+    public boolean insert(int data) {
+        TreeNode newTreeNode = new TreeNode(data);
+        if(root == null){//当前树为空树，没有任何节点
+            root = newTreeNode;
+            return true;
+        }else{
+            TreeNode current = root;
+            TreeNode parentTreeNode = null;
+            while(current != null){
+                parentTreeNode = current;
+                if(current.val > data){//当前值比插入值大，搜索左子节点
+                    current = current.left;
+                    if(current == null){//左子节点为空，直接将新值插入到该节点
+                        parentTreeNode.left = newTreeNode;
+                        return true;
+                    }
+                }else{
+                    current = current.right;
+                    if(current == null){//右子节点为空，直接将新值插入到该节点
+                        parentTreeNode.right = newTreeNode;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.val);
+        inOrder(root.right);
+
+    }
 }
